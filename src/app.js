df2436899b0349fe5e7e3ca01dbdcd2132889b02
ola-alison <ola.alison@gmail.com>
2018-03-13 00:53:59 +0100

    changed scale, added moving through walls, wip: generate food

--- src/app.js
+++ src/app.js
@@ -1,32 +1,44 @@
 const board = document.getElementById("board");
 const snake = document.getElementById("snake");
 const btnStart = document.getElementById("btn-start");
-let boardSize, snakeSize;
+let userInput, moveThrough, boardSize, snakeSize, food;
+let x, foodx = 0;
+let y, foody = 0;
 
-let x = 0;
-let y = 0;
+
+let createFood = () => {
+  food = document.createElement("div");
+  board.appendChild(food).innerHTML = "";
+
+  foodx = Math.floor( Math.random() * 100 );
+  foody = Math.floor( Math.random() * 100 );
+
+  food.style.display = `block`;
+  food.style.width = `${snakeSize}px`;
+  food.style.height = `${snakeSize}px`;
+  food.style.background = `yellow`;
+  food.style.position = `absolute`;
+  food.style.top = `${foody}px`;
+  food.style.left = `${foodx}px`;
+}
 
 btnStart.onclick = function setSizes() {
-  boardSize = parseInt(document.getElementById("input-board-size").value);
-  snakeSize = parseInt(document.getElementById("input-snake-size").value);
-  if (isNaN(boardSize) || isNaN(snakeSize)) {
-    boardSize = 100;
-    snakeSize = 5;
-  }
+  userInput = parseInt(document.getElementById("input-board-size").value);
+  moveThrough = document.getElementById("move-through").checked;
+  boardSize = userInput * 100;
+  snakeSize = userInput * 5;
 
   board.style.display = `block`;
   board.style.width = `${boardSize}px`;
   board.style.height = `${boardSize}px`;
-  
+
   snake.style.display = `block`;
   snake.style.width = `${snakeSize}px`;
   snake.style.height = `${snakeSize}px`;
+  snake.style.top = `0`;
+  snake.style.left = `0`;
 
-}
-
-
-const play = () => {
-
+  createFood();
 }
 
 document.onkeydown = function(e) {
@@ -35,23 +47,35 @@ document.onkeydown = function(e) {
       console.log('left');
 
       if (x > 0) {
-        x = x - 1 * snakeSize;
+        x = x - snakeSize;
       }
       else {
-        x;
+        if (moveThrough == true) {
+          x = boardSize - snakeSize;
+        }
+        else {
+          x;
+        }
       }
+
       snake.style.left =`${x}px`;
       break;
 
 
     case 39:
       console.log('right');
-      if (x < boardSize-1*snakeSize) {
-        x = x + 1 * snakeSize;
+      if (x < boardSize - snakeSize) {
+        x = x + snakeSize;
       }
       else {
-        x;
+        if (moveThrough == true) {
+          x = 0;
+        }
+        else {
+          x;
+        }
       }
+
       snake.style.left = `${x}px`;
       break;
 
@@ -59,24 +83,39 @@ document.onkeydown = function(e) {
     case 38:
       console.log('up');
       if (y > 0) {
-        y = y - 1 * snakeSize;
+        y = y - snakeSize;
       }
       else {
-        y;
+        if (moveThrough == true) {
+          y = boardSize - snakeSize;
+        }
+        else {
+          y;
+        }
       }
+
       snake.style.top = `${y}px`;
       break;
 
 
     case 40:
       console.log('down');
-      if (y < boardSize-1*snakeSize) {
-          y = y + 1 * snakeSize;
+      if (y < boardSize - snakeSize) {
+        y = y + snakeSize;
       }
       else {
-        y;
+        if (moveThrough == true) {
+          y = 0;
+        }
+        else {
+          y;
+        }
       }
+
       snake.style.top = `${y}px`;
       break;
   }
+
+
+
 };
